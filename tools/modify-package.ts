e50a6f8804700ed5fe70861476e03365c85f276a
Alan Agius <alan.agius@betssongroup.com>
2017-03-27 13:48:19 +0200

    fix(installer): fix issue that installer was not being triggered.

--- tools/modify-package.ts
+++ tools/modify-package.ts
@@ -3,8 +3,8 @@ import { readFileSync, writeFileSync } from "fs";
 const pkgContents = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 const scripts = { ...pkgContents.scripts, ...{
-	postinstall: "node ./dist/installer install",
-	preuninstall: "node ./dist/installer uninstall"
+	install: "node ./dist/installer install",
+	uninstall: "node ./dist/installer uninstall"
 }};
 
 writeFileSync("./package.json", JSON.stringify({
