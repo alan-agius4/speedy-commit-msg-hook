70f004091d057ed82587a17f2974801f5e8ddad5
Alan Agius <alan.agius4@gmail.com>
2017-04-01 07:50:05 +0200

    feat(rules): add `valid-scopes` rule.

--- src/rules/rules.spec.ts
+++ src/rules/rules.spec.ts
@@ -36,6 +36,18 @@ describe("rulesSpec", () => {
 		});
 	});
 
+	describe(rules.validScopes.name, () => {
+		const VALID_TYPES = ["router", "navigation"];
+
+		it("should fail when 'scope' is not valid", () => {
+			expect(rules.validScopes("router service", MESSAGE_PART, VALID_TYPES).failed).toBe(true);
+		});
+
+		it("should not fail when 'scope' is valid", () => {
+			expect(rules.validScopes("router", MESSAGE_PART, VALID_TYPES).failed).toBe(false);
+		});
+	});
+
 	describe(rules.validTypes.name, () => {
 		const VALID_TYPES = ["test", "refactor"];
 
